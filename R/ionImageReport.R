6186e410f71b9352edfff0ae6c89a952140d7d88
gtluu <gtluu912@gmail.com>
2020-05-29 11:22:59 -0500

    fix ionImageReport function

--- R/ionImageReport.R
+++ R/ionImageReport.R
@@ -1,7 +1,7 @@
 ionImageReport <- function(dataset, mz, filedir=getwd(), filename) {
   pdf(paste0(filename, '.pdf'))
   for (i in mz) {
-    print(image(dataset, mz=iplusminus=0.2))
+    print(image(dataset, mz=i, plusminus=0.2))
   }
   dev.off()
 }
