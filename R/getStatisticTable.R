f24d3b06278795da30cc9edd68231a0d5fbe39fe
Shreya Kishore <shreyakishore1998@gmail.com>
2020-06-03 13:46:43 -0400

    Commiting the final version.

--- R/getStatisticTable.R
+++ R/getStatisticTable.R
@@ -1,3 +1,22 @@
+#' Get Statistic Table 
+#' 
+#' Returns statistic table with optimized r,k and s parameters using 
+#' \code{getStatisticTable}
+#' 
+#' @param sscObject should be the output from \code{spatialShrunkenCentroids}
+#' @param sscParams should be a list of the three optimized r,k and s values. 
+#' @param df is a data frame that contains a complete list of the set of r,k,s 
+#' parameters tested
+#' @param index stores the index of the model with the optimized r,k and s values
+#' @param statTable stores the statistic table corresponding to the model with 
+#' the optimized  r,k and s values
+#' @return \code{getStatisticTable} containing features associated with statistics
+#' @examples 
+#' 
+#' getStatisticTable(ssc, sscParams)
+#' 
+#' @export
+
 getStatisticTable <- function(sscObject, sscParams) 
 {
   df <- modelData(sscObject)
